9cd80e4b5dd7b3f196bf01826627c1b7d714d341
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-18 11:06:28 -0800

    setup async function getConversion, setup currency-conversion.js

---
+++ src/currency-conversion.js
@@ -0,0 +1,5 @@
+export default class CurrencyConversion {
+  static getCurrencyConversion(currAmt, convAmt) {
+    
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import ConversionService from './conversion-service.js';
+import ConversionService from './conversion-service.js';
+import CurrencyConversion from './currency-conversion';
 
 // Business Logic
 
-// async function getConversion(country) {
-//   const response = await ConversionService.getConversion();
-//   if(response.conversion_rates){
-//     printElements(response.conversion_rates.country, country);
-//   }
-// }
+async function getConversion(country, currAmt) {
+  const response = await ConversionService.getConversion();
+  const convAmt = CurrencyConversion.getCurrencyConversion(currAmt);
+  if(response.conversion_rates) {
+    printElements(response.conversion_rates.country, convAmt);
+  } else {
+    printError(response);
+  }
+}
+
+// UI Logid
+
 function handleFormSubmission() {
   event.preventDefault();
   const currAmt = document.getElementById("dollar-conversion").value;
   const country = document.getElementById("country").value;
   console.log(currAmt);
   console.log(country);
-  //document.querySelector('#response').innerText = null;
-  document.querySelector("#exchange-form").reset();
+  document.querySelector("#dollar-conversion").innerText = null;
+  //document.querySelector("#exchange-form").reset();
 }
 
 window.addEventListener("load", function() {
