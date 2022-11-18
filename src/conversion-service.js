7fab52694e9dfa77bdc534b2136f9ab9718b5d3d
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-18 13:24:07 -0800

    debugging errorMessage, and catch error

--- src/conversion-service.js
+++ src/conversion-service.js
@@ -1,9 +1,14 @@
 export default class ConversionService {
   static async getConversion() {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/8d3dfdae23f7737e370899ad/latest/USD`);
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/8d3dfdae23f7737e370899ad/latest/`);
     const jsonifiedResponse = await response.json();
+    console.log(response);
     if (!response.ok) {
-      const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse['error-type']} ${jsonifiedResponse['extra-info']}`;
+      console.log("here");
+      const errorMessage = `Error ${response.status} ${response.statusText} ${jsonifiedResponse['error-type']} ${jsonifiedResponse['extra-info']}`;
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(errorMessage);
       throw new Error(errorMessage);
     }
     return jsonifiedResponse;
