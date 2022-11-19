9ccdfc2cc4917b857b056f2db5eba078f27ede63
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-18 17:05:07 -0800

    store API call in sessionStorage, still working on populating dropdown menu as page loads

--- src/index.js
+++ src/index.js
@@ -2,77 +2,36 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ConversionService from './conversion-service.js';
-import CurrencyConversion from './currency-conversion';
-
-
-// Business Logic
-
-// async function getConversion(country, currAmt) {
-//   const response = await ConversionService.getConversion();
-//   if(response.conversion_rates) {
-//     const convAmt = CurrencyConversion.getCurrencyConversion(currAmt, response.conversion_rates[country]);
-//     printElements(country, convAmt, currAmt);
-//   } else {
-//     printError(response, country);
-//   }
-// }
-
-// // UI Logic
-
-// function printElements(country, conversion, exchangeAmt) {
-//   document.querySelector("#conversion").innerText = `The conversion of ${exchangeAmt} USD to ${country} is ${conversion}.`;
-// }
-
-// function printError(error, country) {
-//   document.querySelector("#conversion").innerText = `There was an error accessing the conversion rate for ${country}: 
-//   ${error}`;
-// }
-
-// function handleFormSubmission() {
-//   event.preventDefault();
-//   const currAmt = document.getElementById("dollar-conversion").value;
-//   const country = document.getElementById("country").value;
-//   console.log(currAmt);
-//   console.log(country);
-//   getConversion(country, currAmt);
-//   document.querySelector("#dollar-conversion").innerText = null;
-//   //document.querySelector("#exchange-form").reset();
-// }
-
-// window.addEventListener("load", function() {
-//   document.getElementById("exchange-form").addEventListener("submit", handleFormSubmission);
-// });
+// import CurrencyConversion from './currency-conversion';
 
 // Business Logic
 
-// async function getConversion() {
-//   const response = await ConversionService.getConversion();
-//   return response;
-// }
-
-// UI Logic
-
-async function makeConversion(country, currAmt) {
+async function makeCall() {
   const response = await ConversionService.getConversion();
   console.log(response);
   if(response.conversion_rates) {
-    const convAmt = CurrencyConversion.getCurrencyConversion(currAmt, response.conversion_rates[country]);
-    printElements(country, convAmt, currAmt);
+    sessionStorage.setItem("apiCall", response);
+    grabResponse();
+    // const convAmt = CurrencyConversion.getCurrencyConversion(currAmt, response.conversion_rates[country]);
+    // printElements(country, convAmt, currAmt);
   } else {
-    printError(response, country);
+    printError(response);
   }
-  sessionStorage.setItem("apiCall", response);
 }
 
-function printElements(country, conversion, exchangeAmt) {
-  document.querySelector("#conversion").innerText = `${exchangeAmt} USD is equal to ${conversion} ${country}`;
+async function grabResponse() {
+  console.log(sessionStorage.getItem("apiCall"));
 }
 
-function printError(error, country) {
-  document.querySelector("#conversion").innerText = `There was an error accessing the conversion rate for ${country}: 
-  ${error}`;
-}
+// function printElements(country, conversion, exchangeAmt) {
+//   document.querySelector("#conversion").innerText = `${exchangeAmt} USD is equal to ${conversion} ${country}`;
+// }
 
+function printError(error) {
+  document.querySelector("#conversion").innerText = `There was an error accessing the conversion rate for : 
+  ${error}`;  
+}     
+// ${country}
 // function populateDropDown() {
 //   //const countries = [];
 //   //const response = await ConversionService.getConversion();
@@ -81,10 +40,9 @@ function printError(error, country) {
 
 function handleFormSubmission() {
   event.preventDefault();
-  const currAmt = document.getElementById("dollar-conversion").value;
-  const country = document.getElementById("country").value;
-  makeConversion(country, currAmt);
-  console.log(sessionStorage.getItem("apiCall"));
+  // const currAmt = document.getElementById("dollar-conversion").value;
+  // const country = document.getElementById("country").value;
+  makeCall();
   document.querySelector("#dollar-conversion").innerText = null;
   //document.querySelector("#exchange-form").reset();
 }
